02fa2ca517bf83fb0a18631465b8a43d5b26bcd5
Daniel Diaz <danieldiazseminario@gmail.com>
2025-11-20 11:53:11 -0500

    MEJORAS EN PRODUCTOS Y MODELOS AGREGADOS MANUALMEMTE

--- app/Controllers/Productos.php
+++ app/Controllers/Productos.php
@@ -8,9 +8,29 @@ class Productos extends ResourceController
     protected $format    = 'json'; // Siempre responderemos en JSON
 
     // 1. LISTAR TODOS (GET /productos)
+    // 1. LISTAR PRODUCTOS (Con datos enriquecidos: Marca, País, Serie, Año)
     public function index()
     {
-        return $this->respond($this->model->findAll());
+        $data = $this->model
+            // A. SELECCIONAMOS SOLO LO QUE EL CLIENTE DEBE VER (Filtro)
+            // Datos del Producto
+            ->select('productos.id, productos.modelo, productos.precio, productos.stock, productos.garantia_meses')
+            // Datos de la Serie (Contexto: Gamer, Militar, Año)
+            ->select('series.nombre_serie, series.publico_objetivo, series.anio_lanzamiento')
+            // Datos del Fabricante (Origen: Nombre, País) -> OJO: No pedimos email ni web
+            ->select('fabricantes.nombre_empresa, fabricantes.pais_origen')
+            
+            // B. HACEMOS LAS UNIONES (Conectamos las tablas)
+            // Unimos Producto con Serie (El Hijo con el Padre)
+            ->join('series', 'series.id = productos.id_serie')
+
+            // Unimos Serie con Fabricante (El Padre con el Abuelo)
+            ->join('fabricantes', 'fabricantes.id = series.id_fabricante')
+
+            // C. TRAEMOS TODO
+            ->findAll();
+
+        return $this->respond($data);
     }
 
     // 2. VER UNO SOLO (GET /productos/{id})
